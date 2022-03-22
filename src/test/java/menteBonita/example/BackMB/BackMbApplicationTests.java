4c9a0e21a727e10f6b1856eed5fa0034998d62c3
unknown <180001055@cientifica.edu.pe>
2022-03-22 12:02:58 -0500

    Modificando Daysi

---
+++ src/test/java/menteBonita/example/BackMB/BackMbApplicationTests.java
@@ -0,0 +1,13 @@
+package menteBonita.example.BackMB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackMbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
